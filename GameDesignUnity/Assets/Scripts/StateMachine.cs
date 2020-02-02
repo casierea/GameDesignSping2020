7752c0f58de8557fba36b4b890d147d4e8e9a260
Casie <dancingmongoose@gmail.com>
2020-02-02 13:22:32 -0700

    PowerUpStates, Statemachine

---
+++ GameDesignUnity/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class StateMachine : MonoBehaviour{
+    public string GameState;
+    public UnityEvent Starting, Playing, Ending, Dying, Winning;
+	
+//StateMachine.Can do a unityevent.
+	
+// Use this for initialization
+    void Start ()
+    {
+        Run();
+    }
+
+    public void ChangeState(string NewState)
+    {
+        GameState = NewState;
+        Run();
+    }
+	
+// Update is called once per frame
+    public void Run () {
+        switch (GameState)
+        {
+            case "Starting":
+                Starting.Invoke();
+                break;
+						
+            case "Dying":
+                Dying.Invoke();
+                break;
+						
+            case "Playing":
+		  
+                Playing.Invoke();
+                break;
+						
+            case "Ending":
+                Ending.Invoke();
+                break;
+					
+            case "Winning":
+                Winning.Invoke();
+                break;
+        }
+    }
+}
+
+
