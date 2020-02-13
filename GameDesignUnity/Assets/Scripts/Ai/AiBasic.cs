40d24565baaa0f641655181c7958729eef3889a7
Unknown <dancingmongoose@gmail.com>
2020-02-12 17:30:37 -0700

    save point before API upgrade

---
+++ GameDesignUnity/Assets/Scripts/Ai/AiBasic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiBasic : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Player, destination;
+
+    private void Start()
+    {
+        destination = Player;
+        agent = GetComponent<NavMeshAgent>;
+    }
+
+    private void Update()
+    {
+        agent.destination = destination.Player;
+        destination = Player;
+    }
+}
