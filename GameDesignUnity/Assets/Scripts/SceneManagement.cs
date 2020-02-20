f19c85b592585554392c4e8d3fc6fcdcc3f9619f
Casie <dancingmongoose@gmail.com>
2020-02-20 10:08:30 -0700

    scene changer, push cube, remake of quartz flower

---
+++ GameDesignUnity/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class SceneManagement : ScriptableObject
+{
+   public void LoadScene (Object sceneName)
+   {
+      var newName = sceneName.name;
+      SceneManager.LoadScene(newName);
+    } 
+}
