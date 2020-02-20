9444cd807ea6a696723f89da1fe5049457a8ffbb
Unknown <dancingmongoose@gmail.com>
2020-02-19 17:18:52 -0700

    health bar progress wip

---
+++ GameDesignUnity/Assets/Scripts/Health/HealthPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthPlayer : MonoBehaviour
+{
+
+    public int maxHealth = 20;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
