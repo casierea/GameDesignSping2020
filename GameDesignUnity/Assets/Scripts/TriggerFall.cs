26d7bb58e0a5500a375d519c8f1b753770795538
Casie <dancingmongoose@gmail.com>
2020-02-25 14:48:09 -0700

    trigger fall

---
+++ GameDesignUnity/Assets/Scripts/TriggerFall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class TriggerFalls: MonoBehaviour
+{
+	
+    private bool away = false;
+    private float awayspeed = 0;
+	
+
+    void OnTriggerEnter(Collider touch)
+    {
+        if (touch.gameObject.name == "Player")
+        {
+            away = true;
+			
+        }
+    }
+
+    void Update()
+    {
+        if (away)
+        {
+            awayspeed += Time.deltaTime;
+            transform.position = new Vector3 (transform.position.x, transform.position.y - awayspeed,
+                transform.position.z);
+				
+        }
+    }
+		
+}
