cb07dcb54662618b559a81171330cf04d4fd09eb
Unknown <dancingmongoose@gmail.com>
2020-02-24 21:53:42 -0700

    boss fight, wall jump up and down

--- GameDesignUnity/Assets/Scripts/Character/CMover.cs
+++ GameDesignUnity/Assets/Scripts/Character/CMover.cs
@@ -14,6 +14,7 @@ public class CMover : MonoBehaviour
 	
 	
     void Start()
+    
     {
         controller = GetComponent<CharacterController>();
     }
---
+++ GameDesignUnity/Assets/Scripts/Trigger/OnTriggerEnterEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnterEvent : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+
+    public UnityEvent Enable;
+    public UnityEvent Trigger;
+
+    void OnEnable()
+    {
+        //Enable.Invoke();
+        Debug.Log("Enable");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trigger.Invoke();
+        Debug.Log("Enter");
+    }
+}
