84ead11194b456ec43691dcfdd1094a7b5c0f302
Unknown <dancingmongoose@gmail.com>
2020-03-04 13:12:50 -0700

    health up

---
+++ GameDesignUnity/Assets/Scripts/Health/UodateImageValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.Experimental.UIElements;
+
+
+public class UodateImageValue : MonoBehaviour
+{
+
+
+    //public FloatData Data;
+    public float Data;
+    public FloatData PlayerSparkleTotal;
+    private Image image;
+    public GameObject Candle;
+
+    //public FloatData MaxAmount;
+
+    //public FloatData currentAmount;
+    // Use this for initialization
+    void Start ()
+    {
+        image = GetComponent<Image>();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        //Data = PlayerObject.GetComponent<ObjectHealth>().CurrentHealth;
+        image.fillAmount = Candle.GetComponent<LightBehaviour>().lossRate;//Data;
+    }
+}
