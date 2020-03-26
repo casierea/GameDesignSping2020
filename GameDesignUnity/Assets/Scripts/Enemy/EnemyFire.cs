979067582ec55533b237ffed4d0f33ba9fb2efd4
Unknown <dancingmongoose@gmail.com>
2020-03-26 16:03:46 -0600

    shooty robot

---
+++ GameDesignUnity/Assets/Scripts/Enemy/EnemyFire.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+
+{
+    public Transform WeaponPosition;
+    public GameObject EnemyShoot;
+    public FloatData BulletSpeed;
+    private float ShootyForce;
+    private bool FacingRight;
+    private bool readyToFire = true;
+	
+    //private void Update()
+    //{
+       // FacingRight = gameObject.GetComponentInParent<SpriteRenderer>().flipX;
+    //}
+
+    private void OnTriggerStay(Collider other)
+    {	//Debug.Log("Player tag");
+		
+        if (other.tag == "Player" & readyToFire)
+        {	
+            Invoke("BeginShoot", 0.5f); 
+            Debug.Log("After begin Shoot" + readyToFire);
+            StartCoroutine(ShootDelay(1.0f));
+            Debug.Log("After begin Delay" + readyToFire);
+        }
+    }
+
+    private IEnumerator ShootDelay(float waitTime)
+    {
+        readyToFire = false;
+        yield return new WaitForSeconds(waitTime);
+        readyToFire = true; 
+        Debug.Log("After ready set true" + readyToFire);
+    }
+
+    void BeginShoot()
+    {
+		
+        if (FacingRight == false)
+        {
+            ShootyForce = -BulletSpeed.Value;
+        }
+        else
+        {
+            ShootyForce = BulletSpeed.Value;
+        }
+        GameObject Clone;
+        Clone = Instantiate(EnemyShoot, WeaponPosition.position, transform.rotation);
+        Clone.GetComponent<Rigidbody>().AddForce(ShootyForce,0,0);
+		
+    }
+		
+
+}
---
+++ GameDesignUnity/Assets/Scripts/Enemy/WaitAndDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitAndDestroy : MonoBehaviour
+{
+    public float bulletLastsSeconds = 3;
+    // Use this for initialization
+    IEnumerator Start () {
+        yield return new WaitForSeconds(bulletLastsSeconds);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Destroy(other.gameObject); 
+	
+        Destroy(gameObject);
+		
+        // makes bullets collide and destory eachother
+    }
+}
