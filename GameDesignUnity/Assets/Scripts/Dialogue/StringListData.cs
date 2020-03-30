550fa4d0a29913964d8142e4ba9a234c7e777b54
Unknown <dancingmongoose@gmail.com>
2020-03-30 13:53:40 -0600

    dialogue Intro. NPC intro

--- GameDesignUnity/Assets/Scripts/Dialogue/StringListData.cs
+++ GameDesignUnity/Assets/Scripts/Dialogue/StringListData.cs
@@ -1,55 +1,29 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
-
 [CreateAssetMenu]
 public class StringListData : ScriptableObject
 {
     public List<string> stringListObj;
 
     public int currentLineNumber;
+    //whatever this is in the editot, it will calll that line
 
-    
-    void Reset()
-        //public void Start()
-    {
-       
-        ; 
-        
-        Debug.Log("strart");
-        currentLineNumber = 0; 
-        Debug.Log("current line number 0");
-    }
-    
     public string ReturnCurrentLine()   //being called constantly. being called by text obj.
     {
-        //check if current line number >= max line count
-        // if greater, delete object, don't do next return
-        if (currentLineNumber <= stringListObj.Count)
-        {
-            return stringListObj[currentLineNumber];    
-        }
-
-        else 
-        {
-
-            return String.Empty;
-      
-        }  
+        return stringListObj[currentLineNumber];
     }
 
-    public void LineNumber()  //being called by button. Part of original code
+    public void LineNumber()  //being called by button
     {
         if (currentLineNumber < stringListObj.Count-1)
         {
             currentLineNumber++;
         }
-        else 
+        else
         {
-            //currentLineNumber = 0; This makes the string loop. 
-        } 
+            currentLineNumber = 0;
+        }
+        
     }
 }
