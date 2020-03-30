6586280f4753a2b17b1b70da1061d3e9ea9b5259
Unknown <dancingmongoose@gmail.com>
2020-03-30 10:59:43 -0600

    button script wont destroy button

--- GameDesignUnity/Assets/Scripts/Dialogue/StringListData.cs
+++ GameDesignUnity/Assets/Scripts/Dialogue/StringListData.cs
@@ -1,29 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
 [CreateAssetMenu]
 public class StringListData : ScriptableObject
 {
     public List<string> stringListObj;
 
     public int currentLineNumber;
-    //whatever this is in the editot, it will calll that line
 
+    //public GameObject buttonImage;
+
+    private void Start()
+    {
+        currentLineNumber = 0; 
+    }
+    
     public string ReturnCurrentLine()   //being called constantly. being called by text obj.
     {
-        return stringListObj[currentLineNumber];
+        //check if current line number >= max line count
+        // if greater, delete object, don't do next return
+        if (currentLineNumber <= stringListObj.Count)
+        {
+            return stringListObj[currentLineNumber];    
+        }
+
+        else 
+        {
+
+            return String.Empty;
+       
+        }  
     }
 
-    public void LineNumber()  //being called by button
+    public void LineNumber()  //being called by button. Part of original code
     {
         if (currentLineNumber < stringListObj.Count-1)
         {
             currentLineNumber++;
         }
-        else
+        else 
         {
-            currentLineNumber = 0;
-        }
-        
+            //currentLineNumber = 0; This makes the string loop. 
+        } 
     }
 }
