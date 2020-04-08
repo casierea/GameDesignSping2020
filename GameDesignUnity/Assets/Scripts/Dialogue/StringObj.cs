f9a9d5af78fed0ab0b26bf841e84a39f44f39226
Unknown <dancingmongoose@gmail.com>
2020-04-08 14:14:17 -0600

    door says, get the key

---
+++ GameDesignUnity/Assets/Scripts/Dialogue/StringObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class StringObj : ScriptableObject
+{
+    public List<string> stringy;
+    public float time; 
+
+ public float objLastsTime = 3;
+    public ScriptableObject gameObject;
+    
+ IEnumerator Start()
+ {
+  yield return new WaitForSeconds(objLastsTime);
+  Destroy(gameObject);
+ }
+}
+
+
+
+
