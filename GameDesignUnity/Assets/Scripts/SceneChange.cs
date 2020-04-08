f83387b27c57e518fff21324f1822c6989ea3ab0
Unknown <dancingmongoose@gmail.com>
2020-04-07 19:59:09 -0600

    scene changer. pause. menu. how to play

---
+++ GameDesignUnity/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void GoToNextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
