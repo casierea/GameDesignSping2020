3877add78a2c84dd8e1bc281a81e7e17201d1d71
Unknown <dancingmongoose@gmail.com>
2020-04-09 19:28:38 -0600

    floating scripts. delete stupid maze

---
+++ GameDesignUnity/Assets/Scripts/Float/FloatUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+// Makes objects float up & down while gently spinning.
+public class FloatUp : MonoBehaviour {
+    // User Inputs
+    public float derpPerSecond = 15.0f;
+    public float speed = 0.5f;
+    public float Continue = 1f;
+ 
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3 ();
+    Vector3 tempPos = new Vector3 ();
+ 
+    // Use this for initialization
+    void Start () {
+        // Keep the position
+        posOffset = transform.position;
+    }
+     
+
+    void Update () {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * derpPerSecond, 0f), Space.World);
+ 
+        //Float up and then down
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * Continue) * speed;
+ 
+        transform.position = tempPos;
+    }
+}
