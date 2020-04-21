762e3ef60bf282593c2c15d142b15871765e5312
Unknown <dancingmongoose@gmail.com>
2020-04-21 14:44:17 -0600

    trouble shooting. SB robot does not see either player

---
+++ GameDesignUnity/Assets/Scripts/Ai/Ai.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Ai : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform destinationObj;
+    private Transform currentDestinationObj;
+    public float restartTime = 3f;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentDestinationObj = destinationObj;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentDestinationObj = transform;
+        Invoke(nameof(RestartAI), restartTime);
+    }
+
+    private void RestartAI()
+    {   
+        currentDestinationObj = destinationObj;
+    }
+
+    void Update()
+    {
+        agent.destination = currentDestinationObj.position;
+    }
+}
