53810954fcdc40beaabfdacf8cbbd98a39976e7c
Unknown <dancingmongoose@gmail.com>
2020-04-26 14:23:26 -0600

    Ai Thurster health boss script

---
+++ GameDesignUnity/Assets/Scripts/Ai/ThurusterHealth.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class ThurusterHealth : MonoBehaviour
+
+
+{
+    public float maxHealth = 20f;
+    public float currentHealth;
+
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+     
+    }
+
+    void Update()
+    {
+        onZeroHealth();
+        
+    }
+
+    public void ChangeHealth(float delta)
+    {
+        currentHealth += delta;
+
+    }
+
+    private void onZeroHealth ()
+    {
+
+        if (currentHealth <= 0)
+        {
+            (gameObject.GetComponent(typeof(Collider)) as Collider).isTrigger = false;
+            Destroy(gameObject);
+            // gameObject.GetComponentInParent<BossNav>().thruster1;
+        }
+
+    }
+
+
+
+}
